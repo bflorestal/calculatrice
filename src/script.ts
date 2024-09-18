9d8923958dabde8d655ca6aa86b225678c000e0f
bflorestal <yayou7710@hotmail.fr>
2024-09-19 01:56:10 +0200

    update code changes

--- src/script.ts
+++ src/script.ts
@@ -2,272 +2,261 @@
 const prevOperandText = document.querySelector("[data-previous-operand]");
 const currentOperandText = document.querySelector("[data-current-operand]");
 if (!prevOperandText || !currentOperandText) {
-    throw new Error("Unexpected error: Missing operand element");
+  throw new Error("Unexpected error: Missing operand element");
 }
 const errorText = document.querySelector("[data-error]");
 if (!errorText) {
-    throw new Error("Unexpected error: Missing error element");
+  throw new Error("Unexpected error: Missing error element");
 }
 
 // ----- Boutons
-// Ligne 1
 const percentBtn = document.querySelector("[data-percent]");
 const resetCurrentBtn = document.querySelector("[data-ce]");
 const resetBtn = document.querySelector("[data-c]");
 const deleteBtn = document.querySelector("[data-delete]");
 
-if (!resetBtn || !resetCurrentBtn) {
-    throw new Error("Unexpected error: Missing reset button element(s)");
-}
-// Ligne 2
 const invertBtn = document.querySelector("[data-invert]");
 const squareBtn = document.querySelector("[data-square]");
 const squareRootBtn = document.querySelector("[data-sqrt]");
-// Opérateurs
 const operatorBtn = document.querySelectorAll("[data-operator]");
-// Opposé (±)
 const negateBtn = document.querySelector("[data-negate]");
-// Afficher le résultat
 const resultBtn = document.querySelector("[data-result]");
-// Chiffres + point
 const operands = document.querySelectorAll("[data-num]");
 const dotBtn = document.querySelector("[data-dot]");
-//
-let prevOperand = prevOperandText.textContent;
-let currentOperand = currentOperandText.textContent;
-let operation: string | undefined;
-// Mode erreur
+
+type Operation = "+" | "-" | "x" | "/";
+
+let prevOperand = prevOperandText.textContent || "";
+let currentOperand = currentOperandText.textContent || "0";
+let operation: Operation | undefined;
 let hasError = false;
 
 // ----- Fonctions
 function addNumber(number: string) {
-    if (currentOperand?.includes(".") && number === ".") return;
-    if (currentOperand === "0" && number === "0") return;
+  if (currentOperand?.includes(".") && number === ".") return;
+  if (currentOperand === "0" && number === "0") return;
 
-    if (currentOperand === "0" && number === ".") {
-        currentOperand += number;
-        return;
-    }
+  if (currentOperand === "0" && number === ".") {
+    currentOperand += number;
+    return;
+  }
 
-    currentOperand === "0" ? currentOperand = number : currentOperand += number;
+  currentOperand === "0"
+    ? (currentOperand = number)
+    : (currentOperand += number);
 }
 
 function displayNumber() {
-    if (!currentOperandText) return;
-    currentOperandText.textContent = currentOperand;
+  if (!currentOperandText) return;
+  currentOperandText.textContent = currentOperand;
 
-    // Si l'utilisateur choisit un opérateur, affiche l'opérande précédente et l'opérateur
-    if (operation !== undefined) {
-        prevOperandText && (prevOperandText.textContent = `${prevOperand} ${operation}`);
-    } else {
-        // prevOperandText && (prevOperandText.textContent += ` ${prevOperand}`);
-        prevOperandText && (prevOperandText.textContent = prevOperand);
-    }
+  if (operation !== undefined) {
+    prevOperandText &&
+      (prevOperandText.textContent = `${prevOperand} ${operation}`);
+  } else {
+    prevOperandText && (prevOperandText.textContent = prevOperand);
+  }
 }
 
-function operationSelection(operator: string) {
-    // S'il y a quelque chose en haut, effectuer l'opération
-    if (prevOperand !== "") {
-        calculatorOperation();
-    }
-    operation = operator;
-    prevOperand = currentOperand;
+function isValidOperation(value: string): value is Operation {
+  return value === "+" || value === "-" || value === "x" || value === "/";
+}
+
+function selectOperation(operator: Operation) {
+  if (prevOperand !== "") {
+    calculatorOperation();
+  }
+  operation = operator;
+  prevOperand = currentOperand;
+  currentOperand = "0"; // réinitialiser l'opérande actuelle après sélection de l'opérateur
 }
 
 function calculatorOperation() {
-    if (!prevOperand || !currentOperand) return;
-    let result;
-    let current = parseFloat(currentOperand);
-    // prevOperand ? prev = parseFloat(prevOperand) : prev = "???"
-    let prev = parseFloat(prevOperand);
+  if (!prevOperand || !currentOperand || !operation) return;
 
-    switch (operation) {
-        case "/":
-            // Erreurs si division par 0
-            if (current === 0) {
-                hasError = true;
-                prev === 0 ? errorMode("Le résultat est indéfini") : errorMode("Désolé... Nous ne pouvons pas diviser par zéro");
-            } else {
-                result = prev / current;
-            }
-            break;
-        
-        case "x":
-            result = prev * current;
-            break;
+  let result;
+  let current = parseFloat(currentOperand);
+  let prev = parseFloat(prevOperand);
 
-        case "-":
-            result = prev - current;
-            break;
-        
-        case "+":
-            result = prev + current;
-            break;
-    }
+  switch (operation) {
+    case "/":
+      if (current === 0) {
+        hasError = true;
+        errorMode("Désolé... Nous ne pouvons pas diviser par zéro");
+        return;
+      }
+      result = prev / current;
+      break;
 
-    if (result) {
-        currentOperand = result.toString();
-        prevOperand = current.toString();
-    }
+    case "x":
+      result = prev * current;
+      break;
 
+    case "-":
+      result = prev - current;
+      break;
+
+    case "+":
+      result = prev + current;
+      break;
+  }
+
+  if (result !== undefined) {
+    currentOperand = result.toString();
+    prevOperand = "";
     operation = undefined;
+  }
 }
 
 function reset() {
-    prevOperand = "";
-    currentOperand = "0";
-    operation = undefined;
+  prevOperand = "";
+  currentOperand = "0";
+  operation = undefined;
+  displayNumber();
 }
 
 function resetCurrent() {
-    currentOperand = "0";
-    displayNumber();
+  currentOperand = "0";
+  displayNumber();
 }
 
 function deleteNumber() {
-    if (!currentOperand || currentOperand === "0") return;
-    // Remettre à 0 s'il n'y a plus qu'un chiffre
-    if (currentOperand.length === 1 || currentOperand === "-0.") {
-        currentOperand = "0";
-        displayNumber();
-        return;
-    }
+  if (!currentOperand || currentOperand === "0") return;
+  if (currentOperand.length === 1 || currentOperand === "-0.") {
+    currentOperand = "0";
+  } else {
     currentOperand = currentOperand.slice(0, -1);
+  }
+  displayNumber();
+}
+
+function percentage() {
+  if (currentOperand) {
+    currentOperand = `${parseFloat(currentOperand) / 100}`;
     displayNumber();
+  }
 }
 
-function percentage(operand: string) {
-    currentOperand = `${parseFloat(operand) / 100}`;
+function invertCurrentNumber() {
+  if (currentOperand && currentOperand !== "0") {
+    currentOperand = `${1 / parseFloat(currentOperand)}`;
+    displayNumber();
+  }
 }
 
-function invertNumber(operand: string) {
-    if (currentOperand === "0") return;
-    currentOperand = `${1 / (parseFloat(operand))}`;
+function calculateSquare() {
+  if (currentOperand) {
+    currentOperand = `${parseFloat(currentOperand) ** 2}`;
+    displayNumber();
+  }
 }
 
-function square(operand: string) {
-    if (prevOperand === "") {
-        prevOperandText && (prevOperandText.textContent = `sqr( ${operand} )`);
-        currentOperand = `${parseFloat(operand) ** 2}`;
-        prevOperand = currentOperand;
-        // ...
-    } else {
-        // ...
-    }
+function calculateSqrt() {
+  if (currentOperand) {
+    currentOperand = `${Math.sqrt(parseFloat(currentOperand))}`;
+    displayNumber();
+  }
 }
 
-function squareRoot(operand: string) {
-    if (prevOperand === "") {
-        prevOperandText && (prevOperandText.textContent = `√( ${operand} )`);
-        currentOperand = `${Math.sqrt(parseFloat(operand))}`;
-        prevOperand = currentOperand;
-        // ...
-    } else {
-        // ...
-    }
-}
-
-// ----- Mode erreur
 function errorMode(error: string) {
-    if (!errorText) return;
-    if (!hasError) return;
-
-    // peut-être à mettre dans une autre fonction, qui ferait que
-    // s'il y a une erreur on désactive, sinon on réactive
-    const buttonsToDisable = [percentBtn, invertBtn, squareBtn, squareRootBtn, ...operatorBtn, negateBtn, dotBtn];
-
-    // Affiche le message d'erreur
+  if (errorText && hasError) {
     errorText.textContent = error;
     currentOperand = "";
+    const buttonsToDisable = [
+      percentBtn,
+      invertBtn,
+      squareBtn,
+      squareRootBtn,
+      ...operatorBtn,
+      negateBtn,
+      dotBtn,
+    ];
+    buttonsToDisable.forEach((btn) => btn?.setAttribute("disabled", ""));
+  }
+}
 
-    // Désactive les boutons sauf reset, delete, chiffres et =
-    // https://bobbyhadz.com/blog/typescript-disable-button
-    buttonsToDisable.forEach(btn => btn?.setAttribute("disabled", ""));
-
-    // Réactiver les boutons et mettre hasError à false au clic sur un bouton de
-    // [...document.querySelectorAll("button")].filter(btn => btn.disabled === false)
-    // hasError &&= false;
+function clearError() {
+  if (hasError) {
+    hasError = false;
+    errorText && (errorText.textContent = "");
+    const buttonsToEnable = [
+      percentBtn,
+      invertBtn,
+      squareBtn,
+      squareRootBtn,
+      ...operatorBtn,
+      negateBtn,
+      dotBtn,
+    ];
+    buttonsToEnable.forEach((btn) => btn?.removeAttribute("disabled"));
+  }
 }
 
 // ----- Événements au clic
-// Boutons reset (CE et C)
-for (const button of [resetBtn, resetCurrentBtn]) {
-    button.addEventListener("click", () => {
-        hasError &&= false;
-        button.textContent === "CE" ? resetCurrent() : reset();
-        displayNumber();
-    })
-}
+[resetBtn, resetCurrentBtn].forEach((button) => {
+  button?.addEventListener("click", () => {
+    clearError();
+    button.textContent === "CE" ? resetCurrent() : reset();
+    displayNumber();
+  });
+});
 
-// Bouton delete ()
 deleteBtn?.addEventListener("click", () => {
-    hasError &&= false;
-    deleteNumber();
-})
+  clearError();
+  deleteNumber();
+});
 
-// Bouton pourcentage (%)
 percentBtn?.addEventListener("click", () => {
-    currentOperand && (percentage(currentOperand));
-    displayNumber();
-})
+  clearError();
+  percentage();
+});
 
-// Bouton inverse (1/x)
 invertBtn?.addEventListener("click", () => {
-    currentOperand && (invertNumber(currentOperand));
-    displayNumber();
-})
+  clearError();
+  invertCurrentNumber();
+});
 
-// Bouton carré (²)
 squareBtn?.addEventListener("click", () => {
-    currentOperand && (square(currentOperand));
-    displayNumber();
-})
+  clearError();
+  calculateSquare();
+});
 
-// Bouton racine carrée (√x)
 squareRootBtn?.addEventListener("click", () => {
-    currentOperand && (squareRoot(currentOperand));
+  clearError();
+  calculateSqrt();
+});
+
+operands.forEach((operand) => {
+  operand.addEventListener("click", () => {
+    clearError();
+    addNumber(operand.textContent || "");
     displayNumber();
-})
+  });
+});
 
-// Chiffre ou point
-operands.forEach(operand => {
-    operand.addEventListener("click", () => {
-        hasError &&= false;
-        operand.textContent && (addNumber(operand.textContent));
-        // Affiche le chiffre à l'écran
-        displayNumber();
-    })
-})
+operatorBtn.forEach((button) => {
+  button.addEventListener("click", () => {
+    clearError();
 
-// Opérateurs
-operatorBtn.forEach(button => {
-    button.addEventListener("click", () => {
-        button.textContent && (operationSelection(button.textContent));
-        displayNumber();
-    })
-})
+    const operation = button.textContent || "";
+    if (isValidOperation(operation)) {
+      selectOperation(operation);
+      displayNumber();
+    }
+  });
+});
 
-// Bouton opposé (±)
 negateBtn?.addEventListener("click", () => {
-    if (!currentOperand || currentOperand === "0") return;
+  if (!currentOperand || currentOperand === "0") return;
+  currentOperand =
+    currentOperand.charAt(0) !== "-"
+      ? `-${currentOperand}`
+      : currentOperand.slice(1);
+  displayNumber();
+});
 
-    // if (prevOperand === "") {
-        // Si le nombre est positif, il devient négatif
-        if (currentOperand.charAt(0) !== "-") {
-            currentOperand = currentOperand.replace(/^/gm, "-");
-        // Si le nombre est négatif, il devient positif
-        } else if (currentOperand.charAt(0) === "-") {
-            currentOperand = currentOperand.replace(/^-/gm, "");
-        }
-    // } else {
-        // ... 
-    // }
-
-    displayNumber();
-})
-
-// Bouton égal (=)
 resultBtn?.addEventListener("click", () => {
-    hasError ? hasError = false : calculatorOperation();
-    displayNumber();
-})
+  if (hasError) return;
+  calculatorOperation();
+  displayNumber();
+});
